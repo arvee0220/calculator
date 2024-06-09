8ad253fc8e47eba3b4940db4f55e1a4816b37949
arvee0220 <rowellverdejo@gmail.com>
2024-06-09 11:41:14 -0400

    Push project into github repo

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import "./App.scss";
+
+const App = () => {
+	const [value, setValue] = useState("");
+
+	const valueHandler = (e) => {
+		e.preventDefault();
+		setValue(value + e.target.value);
+	};
+
+	const resetHandler = () => {
+		setValue("");
+	};
+
+	const deleteValue = () => {
+		setValue(value.slice(0, -1));
+	};
+
+	const computeValue = () => {
+		const expression = value.replace(/\^/g, "**");
+		setValue(eval(expression));
+	};
+
+	return (
+		<div className="container">
+			<div className="calculator">
+				<form action="">
+					<div className="display">
+						<input type="text" value={value} readOnly />
+					</div>
+					<div>
+						<input className="" type="button" value="AC" onClick={resetHandler} />
+						<input className="" type="button" value="DE" onClick={deleteValue} />
+						<input className="" type="button" value="." onClick={valueHandler} />
+						<input className="" type="button" value="/" onClick={valueHandler} />
+					</div>
+					<div>
+						<input className="" type="button" value="7" onClick={valueHandler} />
+						<input className="" type="button" value="8" onClick={valueHandler} />
+						<input className="" type="button" value="9" onClick={valueHandler} />
+						<input className="" type="button" value="*" onClick={valueHandler} />
+					</div>
+					<div>
+						<input className="" type="button" value="4" onClick={valueHandler} />
+						<input className="" type="button" value="5" onClick={valueHandler} />
+						<input className="" type="button" value="6" onClick={valueHandler} />
+						<input className="" type="button" value="+" onClick={valueHandler} />
+					</div>
+					<div>
+						<input className="" type="button" value="1" onClick={valueHandler} />
+						<input className="" type="button" value="2" onClick={valueHandler} />
+						<input className="" type="button" value="3" onClick={valueHandler} />
+						<input className="" type="button" value="-" onClick={valueHandler} />
+					</div>
+					<div>
+						<input className="" type="button" value="00" onClick={valueHandler} />
+						<input className="" type="button" value="0" onClick={valueHandler} />
+						<input className="" type="button" value="^" onClick={valueHandler} />
+						<input className="equal" type="button" value="=" onClick={computeValue} />
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default App;
